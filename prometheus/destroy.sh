6ffa0837cc12a58a8d80e35b892cc54bdc5d3e2e
Adam Power <apower@pivotal.io>
2020-04-14 17:10:27 +0100

    Updated destroy script to clean up more objects

--- prometheus/destroy.sh
+++ prometheus/destroy.sh
@@ -6,3 +6,6 @@ script_dir="$(cd "$(dirname "$0")"; pwd -P)"
 
 kubectl delete --ignore-not-found -f "${script_dir}/prometheus-deployment.yml"
 kubectl delete --ignore-not-found -f "${script_dir}/prometheus-configmap-basic.yml"
+kubectl delete --ignore-not-found -f "${script_dir}/bosh-exporter-deployment.yml"
+kubectl delete --ignore-not-found secret bosh-exporter-env
+kubectl delete --ignore-not-found secret bosh-exporter-ca
